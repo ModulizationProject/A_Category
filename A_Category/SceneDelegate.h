9de37e4834ef533e3519ed6def5b5f85fe1d710f
zpr <2337262844@qq.com>
2021-07-09 16:27:52 +0800

    first commit

---
+++ A_Category/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  A_Category
+//
+//  Created by 朱浦睿 on 2021/7/9.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ A_Category/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  A_Category
+//
+//  Created by 朱浦睿 on 2021/7/9.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
